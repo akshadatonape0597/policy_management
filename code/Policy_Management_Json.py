6b7456ac55be5c23f50502d3b856d18533c8e45d
Akshada Tonape <akshada_tonape@external.mckinsey.com>
2022-03-30 19:34:16 +0530

    first commit

---
+++ code/Policy_Management_Json.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+
+
+if __name__=="__main__":
+    d={}
+    df=pd.read_excel('C:/Users/Akshada Tonape/OneDrive - McKinsey & Company/Desktop/Python/inbound/input.xlsx')
+    for k,g in df.groupby("Cat nbr"):
+        d[k]=g['Resource ']
+    for key, value in d.items():    
+        name="pn:promo:LIVE:"+ str(key).zfill(3)
+        description="Policy for "+str(key).zfill(3) +" in LIVE dataset. These are just sample resources and actions. This is not a complete list"
+        subject="gn:promo:LIVE:"+str(key).zfill(3)
+        effect="allow"
+        resources=[]
+        for resource in  d[key]:
+            resources.append("rn:promo:LIVE:subcategory:"+str(resource)+":*")
+        
+        data={
+            "name":name,
+            "description":description,
+            "subjects":[subject],
+            "effect":effect,
+            "resources":resources,
+            "actions": [
+                "an:promo:LIVE:dummy:state:0:all:NA"
+            ],
+            "conditions": {},
+            "meta": 'NULL'
+
+
+        }
+        
+        with open('outbound\pn-promo-live-'+str(key)+'.json', 'w') as outfile:
+            json.dump(data, outfile,separators=(',\n', ': '))
---
+++ test.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df=pd.read_excel('C:/Users/Akshada Tonape/OneDrive - McKinsey & Company/Desktop/Python/input.xlsx')
+ 

f8718420b426622e7ed7187548dfb8c6d2856552
Akshada Tonape <akshada_tonape@external.mckinsey.com>
2022-03-30 20:20:39 +0530

    second commit

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+
 import pandas as pd
-df=pd.read_excel('C:/Users/Akshada Tonape/OneDrive - McKinsey & Company/Desktop/Python/input.xlsx')
- 
+path="/inbound/input.xlsx"
+df=pd.read_excel(path)
+print(df.head(9))
+
+ #'C:/Users/Akshada Tonape/OneDrive - McKinsey & Company/Desktop/Python/input.xlsx

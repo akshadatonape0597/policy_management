313f489af5736fca422a66818f8f1319242f3ede
akshadatonape0597 <102675088+akshadatonape0597@users.noreply.github.com>
2022-03-31 14:59:20 +0530

    second commit

--- code/Policy_Management_Json.py
+++ code/Policy_Management_Json.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import json
-
+import sys
 
 if __name__=="__main__":
     d={}
-    df=pd.read_excel('C:/Users/Akshada Tonape/OneDrive - McKinsey & Company/Desktop/Python/inbound/input.xlsx')
+    df=pd.read_excel(sys.argv[1])
     for k,g in df.groupby("Cat nbr"):
         d[k]=g['Resource ']
     for key, value in d.items():    
@@ -31,5 +31,5 @@ if __name__=="__main__":
 
         }
         
-        with open('outbound\pn-promo-live-'+str(key)+'.json', 'w') as outfile:
+        with open(sys.argv[2]+'pn-promo-live-'+str(key)+'.json', 'w') as outfile:
             json.dump(data, outfile,separators=(',\n', ': '))
